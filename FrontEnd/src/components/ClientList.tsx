07ed6db02060aa0ceff2846420095eb0f10ddb24
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2025-02-05 17:34:15 -0300

    fIX: some ajusts

--- FrontEnd/src/components/ClientList.tsx
+++ FrontEnd/src/components/ClientList.tsx
@@ -3,7 +3,7 @@ import { deleteUser } from '../hooks/AsyncDados';
 
 
 const ClientList = ({ dados }: { dados: Clients[] }) => {
-    
+
     const handleDelete = async (clientID: number) => {
         try {
             await deleteUser(clientID.toString());
@@ -19,27 +19,27 @@ const ClientList = ({ dados }: { dados: Clients[] }) => {
             <h4 className="text-center text-gray-700">Gerenciamento dos principais clientes:</h4>
             <div className="flex flex-wrap gap-4 justify-center mt-4">
                 {dados.map((client) => (
-                    <div key={client.id} className="bg-white flex flex-col p-3 w-[278px] rounded-3xl shadow-md">
-                        
-                        <div className="p-4 bg-[#F4F5F7] rounded-b-3xl ">
-                            <button
-                                onClick={() => client.id !== undefined && handleDelete(client.id)}
-                                className="absolute top-2 right-2"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="text-red-400 size-6"
+                    <div className="bg-white flex flex-col p-3 w-[278px] rounded-3xl shadow-md">
+                        <div className="p-4 bg-[#F4F5F7] rounded-b-3xl">
+                            <div className="relative">
+                                <button
+                                    onClick={() => client.id !== undefined && handleDelete(client.id)}
+                                    className="absolute top-2 right-2"
                                 >
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="text-red-400 size-6"
+                                    >
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
                             <h1 className="font-bold text-base mt-3">{client.name}</h1>
                             <div className="my-2 text-sm text-gray-600">Ramo: {client.role}</div>
-                         
                         </div>
                     </div>
                 ))}
@@ -49,3 +49,6 @@ const ClientList = ({ dados }: { dados: Clients[] }) => {
 };
 
 export default ClientList;
+
+
+
